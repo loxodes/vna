4d80478ef8c53b2279e53f43a3132bdb8810e08f
Jon Klein <arcticmicrowave@gmail.com>
2016-06-19 23:56:24 -0800

    started terrible bit-banged driver to test ltc2323 breakout

---
+++ software/adc/adc_test/adc_test.ino
@@ -0,0 +1,74 @@
+// terrible bit-banged driver for ADC to test hardware..
+
+#define ADC_BITS 16
+#define CNV_DELAY 1
+#define SCK_DELAY 1
+
+// digital pin 5 has a pushbutton attached to it. Give it a name:
+int ADC_CNV_INPUT = 31; // LTC2323 pin 9, high defines sample phase, low starts conversion phase
+int ADC_CNV_CLK_OUT = 32; // LTC2323 conversion clock output
+int ADC_CNV_EN = 33; // LTC2323 conversion enable, high is enabled?
+int ADC_SCK = 34; // LTC2323 pin 21, input spi clock
+int ADC_CLKOUT = 35; // LTC2323 pin 17, output spi clock
+int ADC_SD2 = 36; // LTC2323 pin 19, adc channel 1
+int ADC_SD1 = 37; // LTC2323 pin 15, adc channel 1
+
+
+void ltc2323_init():
+{
+  pinMode(ADC_CNV_INPUT, OUTPUT);
+  pinMode(ADC_CNV_CLK_OUT, INPUT_PULLUP);
+  pinMode(ADC_CNV_EN, OUTPUT);
+  pinMode(ADC_SCK, OUTPUT);
+  pinMode(ADC_CLKOUT, INPUT_PULLUP);
+  pinMode(ADC_SD2, INPUT_PULLUP);
+  pinMode(ADC_SD1, INPUT_PULLUP);
+  
+  digitalWrite(ADC_CNV_EN, HIGH);
+  digitalWrite(ADC_CNV_INPUT, HIGH);
+}
+
+uint32_t ltc2323_conv():
+{
+  uint32_t adc1 = 0;
+  uint32_t adc2 = 0;
+  digitalWrite(ADC_SCK, HIGH);
+  digitalWrite(ADC_CNV_INPUT, HIGH);
+  delayMicroseconds(CNV_DELAY);
+  digitalWrite(ADC_CNV_INPUT, LOW);
+  delayMicroseconds(CNV_DELAY);
+  
+  for(uint32_t i = 0; i < ADC_BITS; i++) {
+    digitalWrite(ADC_SCK, LOW);
+    delayMicroseconds(SCK_DELAY);
+    adc1 |= digitalRead(ADC_SD1);
+    adc2 |= digitalRead(ADC_SD2);
+    adc1 = adc1 << 1;
+    adc2 = adc2 << 1;
+    digitalWrite(ADC_SCK, HIGH);
+    delayMicroseconds(SCK_DELAY);
+  }
+  
+  return adc1 | (adc2 << 16);
+  
+}
+
+void setup() {
+  Serial.begin(9600);
+  ltc2323_init();
+
+}
+
+void loop() {
+  uint32_t sample = ltc2323_conv();
+  uint16_t sdo1 = sample & 0xFFFF;
+  uint16_t sdo2 = sample >> 16;
+  
+  Serial.print("sdo1: ");
+  Serial.print("sdo1: ");
+  Serial.print("sdo1: ");
+  Serial.println("sdo1: ");
+}
+
+
+
