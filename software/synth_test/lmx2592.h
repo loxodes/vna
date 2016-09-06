b12a77483b87ff59d0f215df67a420347c860fa4
Jon Klein <arcticmicrowave@gmail.com>
2016-09-05 18:54:48 -0800

    added missing header

---
+++ software/synth_test/lmx2592.h
@@ -0,0 +1,72 @@
+
+#define BIT0 (1 << 0)
+#define BIT1 (1 << 1)
+#define BIT2 (1 << 2)
+#define BIT3 (1 << 3)
+#define BIT4 (1 << 4)
+#define BIT5 (1 << 5)
+#define BIT6 (1 << 6)
+#define BIT7 (1 << 7)
+#define BIT8 (1 << 8)
+#define BIT9 (1 << 9)
+#define BIT10 (1 << 10)
+#define BIT11 (1 << 11)
+#define BIT12 (1 << 12)
+#define BIT13 (1 << 13)
+#define BIT14 (1 << 14)
+#define BIT15 (1 << 15)
+#define BIT16 (1 << 16)
+#define BIT17 (1 << 17)
+#define BIT18 (1 << 18)
+#define BIT19 (1 << 19)
+#define BIT20 (1 << 20)
+#define BIT21 (1 << 21)
+#define BIT22 (1 << 22)
+#define BIT23 (1 << 23)
+
+#define REG0_LD_EN BIT13
+#define REG0_FCAL_EN BIT3
+#define REG0_RESET BIT1
+#define REG0_MUXOUT_SEL BIT2
+#define REG46_MASH_ORDER_2 2
+#define REG46_MASH_EN BIT5
+#define REG46_OUTA_PD BIT6
+#define REG46_OUTB_PD BIT7
+
+#define REG30_VCO_2X_EN BIT0
+
+#define REG38_PLL_N 1
+#define REG39_PFD_DLY_2 BIT9
+
+#define REG36_CHDIV_DISTB_EN BIT11
+#define REG36_CHDIV_DISTA_EN BIT10
+#define REG36_CHDIV_SEG_SEL_1 BIT4
+#define REG36_CHDIV_SEG_SEL_12 BIT5
+#define REG36_CHDIV_SEG_SEL_123 BIT6
+#define REG36_CHDIV_SEG3 0
+
+
+#define REG35_CHDIV_SEG2 9
+#define REG35_CHDIV_SEG3_EN BIT8
+#define REG35_CHDIV_SEG2_EN BIT7
+#define REG35_CHDIV_SEG1 2 
+#define REG35_CHDIV_SEG1_EN BIT1
+
+#define REG34_CHDIV_EN BIT5
+
+#define REG48_OUTB_MUX_VCO BIT0
+#define REG48_OUTB_MUX_DIV 0
+
+#define REG10_MULT 7
+#define REG11_PLL_R 4
+#define REG12_PLL_R_PRE 0
+#define REG47_OUTB_POW 0
+#define REG46_OUTA_POW 8
+
+#define REG47_OUTA_MUX_DIV 0
+#define REG47_OUTA_MUX_VCO BIT11
+
+
+#define REG31_VCO_DISTA_PD BIT9
+#define REG31_VCO_DISTB_PD BIT10
+#define REG31_CHDIV_DIST_PD BIT7
