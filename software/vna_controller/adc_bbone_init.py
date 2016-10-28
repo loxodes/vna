4b7fa1a51fb8fbd3d116ba758b6422d409bc850c
Jon Klein <arcticmicrowave@gmail.com>
2016-10-28 01:43:01 -0800

    .. actually added script to initalize ad9864

---
+++ software/vna_controller/adc_bbone_init.py
@@ -0,0 +1,109 @@
+# script to bit-bang ad9864 initialiation over SPI for fixed 45 MHz IF 
+# (use bit-banged SPI, initialization adc initialization only happens once
+# save the hardware SPI for more exciting things like controlling the synth)
+# currently hardcoded for assuming 26 MHz ref and adc clk
+
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+
+class bitbang_spi:
+   def init(self, spi_cs, spi_mosi, spi_miso, spi_clk):
+        self.spi_cs = spi_cs
+        self.spi_mosi = spi_mosi
+        self.spi_miso = spi_miso
+        self.spi_clk = spi_clk
+
+        GPIO.setup(spi_cs, GPIO.OUT)
+        GPIO.setup(spi_mosi, GPIO.OUT)
+        GPIO.setup(spi_clk, GPIO.OUT)
+        GPIO.setup(spi_miso, GPIO.IN)
+        GPIO.output(spi_cs, GPIO_HIGH)
+    
+    def transfer(payload, bits = 8):
+        GPIO.output(spi_cs, GPIO_LOW)
+        GPIO.output(spi_clk, GPIO_LOW)
+
+        response = 0
+        for i in range(bits):
+            # data clocked in on clock rising edge
+            GPIO.output(spi_mosi, (payload >> i) & 0x01)
+            GPIO.output(spi_clk, GPIO_HIGH)
+            response |= GPIO.input(spi_miso) 
+            response = response << 1
+            GPIO.output(spi_clk, GPIO_LOW)
+
+        GPIO.output(spi_cs, GPIO_HIGH)
+
+        return response
+
+
+def ad9864_write_reg(spi, addr, val):
+    payload = addr << 9 | val
+    spi.transfer(payload, bits = 16)
+
+def ad9864_read_reg(spi, addr):
+    AD9864_READ_MASK = ((1 << 15))
+
+    payload = addr << 9 | AD9864_READ_MASK;
+    response = spi.transfer(payload, bits = 16)
+
+    return response & 0xFF
+
+def ad9864_init(spi):
+    ad9864_write_reg(spi, 0x3F, 0x99) # software reset
+    ad9864_write_reg(spi, 0x19, 0x87) # 4-wire SPI, 16 bit I/Q
+
+    ad9864_write_reg(spi, 0x00, 0x77) # take ref out of standby
+
+    # lc and rc resonator calibration
+    ad9864_write_reg(spi, 0x3E, 0x47)
+    ad9864_write_reg(spi, 0x38, 0x01)
+    ad9864_write_reg(spi, 0x39, 0x0F)
+    time.sleep(.001)    
+
+    for i in range(5):
+        ad9864_write_reg(spi, 0x1C, 0x03)
+        ad9864_write_reg(spi, 0x00, 0x74)
+        time.sleep(.006)
+        r = ad9864_read_reg(spi, 0x1C)
+
+        if r == 0:
+            print('LC/RC calibration worked!'
+            break
+
+        ad9864_write_reg(spi, 0x1C, 0x00)
+        print("LC/RC calibration failed, retrying..")
+
+    ad9864_write_reg(spi, 0x38, 0x00)
+    ad9864_write_reg(spi, 0x3E, 0x00)
+
+    # lo synth configuration, set LO to 48.25 MHz
+    ad9864_write_reg(spi, 0x00, 0x30) # enable everything but ck..
+    ad9864_write_reg(spi, 0x08, 0x00)  
+    ad9864_write_reg(spi, 0x09, 0x68) # LOR = 104 (so, fif = 250 kHz * (8 LOB + LOA)
+    ad9864_write_reg(spi, 0x0A, 0x20) # LOA = 1
+    ad9864_write_reg(spi, 0x0B, 0x18) # LOB = 24 (0x18)
+    ad9864_write_reg(spi, 0x0C, 0x07) # normal LO charge pump current control
+
+
+    # configure decimation
+    ad9864_write_reg(spi, 0x07, 0x0e) # set decimation rate to 900, 60 * (M + 1) if K = 0, M = 14
+
+    # configure SSI
+    ad9864_write_reg(spi, 0x1A, 0x08) # (clkout freq = fclk / 8)
+
+
+
+if __name__ == '__main__':
+    # TODO: pick pins for spi..
+    ADC_SPI_CS = "P8_N"
+    ADC_SPI_MOSI = "P8_N"
+    ADC_SPI_MISO = "P8_N"
+    ADC_SPI_CLK = "P8_N"
+    # spi clk, spi mosi and spi miso 
+    
+    spi = bitbang_spi_init(ADC_SPI_CS, ADC_SPI_MOSI, ADC_SPI_MISO, ADC_SPI_CLK)
+    ad9864_init(spi)
+
+
