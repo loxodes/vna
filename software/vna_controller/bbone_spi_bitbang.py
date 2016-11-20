a86743cdc3fbc6d79a7bb972ac05e038b07d2fde
Jon Klein <arcticmicrowave@gmail.com>
2016-11-19 18:05:23 -0900

    added missing bit-banged SPI driver..

---
+++ software/vna_controller/bbone_spi_bitbang.py
@@ -0,0 +1,34 @@
+import Adafruit_BBIO.GPIO as GPIO
+
+class bitbang_spi:
+   def __init__(self, spi_cs, spi_mosi, spi_miso, spi_clk):
+        self.spi_cs = spi_cs
+        self.spi_mosi = spi_mosi
+        self.spi_miso = spi_miso
+        self.spi_clk = spi_clk
+
+        GPIO.setup(spi_cs, GPIO.OUT)
+        GPIO.setup(spi_mosi, GPIO.OUT)
+        GPIO.setup(spi_clk, GPIO.OUT)
+        GPIO.setup(spi_miso, GPIO.IN)
+        GPIO.output(spi_cs, GPIO.HIGH)
+
+   def transfer(self, payload, bits = 8):
+        print('sending: {}'.format(hex(payload)))
+        GPIO.output(self.spi_cs, GPIO.LOW)
+        GPIO.output(self.spi_clk, GPIO.LOW)
+
+        response = 0
+        for i in range(bits):
+            response = response << 1
+            # data clocked in on clock rising edge
+            GPIO.output(self.spi_mosi, (payload >> (bits - (i + 1))) & 0x01)
+            GPIO.output(self.spi_clk, GPIO.HIGH)
+            response |= GPIO.input(self.spi_miso)
+            GPIO.output(self.spi_clk, GPIO.LOW)
+
+        GPIO.output(self.spi_cs, GPIO.HIGH)
+
+        return response
+
+
