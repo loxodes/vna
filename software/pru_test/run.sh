ef2868efa5b3348f5168dca91d99ce52d1088c26
Jon Klein <arcticmicrowave@gmail.com>
2016-11-29 08:22:10 +0000

    removed outdated energia drivers and stale measurements

--- software/pru_test/run.sh
+++ software/pru_test/run.sh
@@ -1,5 +1,9 @@
-sudo echo PRU-ADC-R0 > /sys/devices/bone_capemgr.?/slots
-sudo dtc -O dtb -I dts -o /lib/firmware/PRU-ADC-R0-00A0.dtbo -b 0 -@ PRU-ADC-R0-00A0.dts
+# compile and install device tree for pru with the following command:
+#sudo dtc -O dtb -I dts -o /lib/firmware/PRU-ADC-R0-00A0.dtbo -b 0 -@ PRU-ADC-R0-00A0.dts
+
+#for debian 7, add the following to /etc/rc.local: echo PRU-ADC-R0 > /sys/devices/bone_capemgr.?/slots
+#for debian 8, add PRU-ADC-R0 to /etc/default/capemgr
+
 gcc adc_to_socket.c -lprussdrv -o loader
 pasm -b memtest.p
 ./loader memtest.bin
