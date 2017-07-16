35fa3bf9ef7d6d70ebfbb77c9dfafd58ffe72e07
Jon Klein <arcticmicrowave@gmail.com>
2017-07-16 01:26:59 -0800

    added script for converting TICS pro register dumps to python dictionaries

---
+++ software/vna_controller/lmx_tools/lmx_reg_txt_to_array.py
@@ -0,0 +1,31 @@
+# converts txt register dump from TICS Pro into a python dictionary
+
+filename = 'lmx2594_default_regs.txt'
+
+regs = []
+
+REGS_PER_LINE = 8
+
+with open(filename, 'r') as f:
+    for line in f:
+        l = line.split()
+        reg = l[0][1:]
+        value = l[1]
+        regs.append((reg, value))
+
+
+regs.reverse()
+
+print('LMX_DEFAULT_REGS = {', end='')
+
+for i,reg in enumerate(regs):
+    if i % REGS_PER_LINE == 0:
+        print('\\\n\t', end='')
+    print('{}:{}'.format(reg[0], reg[1]),end='')
+    if i != len(regs) - 1:
+        print(', ', end='')
+
+print('}')
+
+
+
