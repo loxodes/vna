2f4b1612264e61d2e4f91428d8e50aacb7e303cd
Jon Klein <arcticmicrowave@gmail.com>
2018-08-26 18:40:20 +0000

    fix author

--- software/vna_controller/mmap_gpio.py
+++ software/vna_controller/mmap_gpio.py
@@ -1,6 +1,6 @@
 # mmap bit-banging io library
 
-# inspirted by:
+# inspired by:
 # https://graycat.io/tutorials/beaglebone-io-using-python-mmap/
 from mmap import mmap
 import time, struct
@@ -14,14 +14,38 @@ class GPIO:
                 for offset in GPIO_offset:
                         self.gpio_mem.append(mmap(f.fileno(), GPIO_size, offset=offset))
 
+    
+    def _read_reg(self, port, offset):
+        return struct.unpack("<L", self.gpio_mem[port][offset:offset+4])[0]
+   
+    def _clear_bit(self, port, offset, bit, mask = True):
+        reg = 0
+        
+        if mask:
+            reg = self._read_reg(port, offset)
+
+        self.gpio_mem[port][offset:offset+4] = struct.pack("<L", reg & ~(1 << bit))
+
+    def _set_bit(self, port, offset, bit, mask = True):
+        reg = 0
+
+        if mask:
+            reg = self._read_reg(port, offset)
+
+        self.gpio_mem[port][offset:offset+4] = struct.pack("<L", reg | (1 << bit))
 
     def set_output(self, gpio):
         port = gpio[0]
         pin = gpio[1]
         GPIO_OE = 0x134
-        reg = struct.unpack("<L", self.gpio_mem[port][GPIO_OE:GPIO_OE+4])[0]
-        self.gpio_mem[port][GPIO_OE:GPIO_OE+4] = struct.pack("<L", reg & ~(1 << pin))
+        self._clear_bit(port, GPIO_OE, pin)
 
+    def set_input(self, gpio): 
+        port = gpio[0]
+        pin = gpio[1]
+        GPIO_OE = 0x134
+        GPIO_DATAOUT = 0x13C
+        self._set_bit(port, GPIO_OE, pin)
 
     def set_value(self, gpio, value):
         port = gpio[0]
@@ -30,12 +54,18 @@ class GPIO:
         GPIO_CLEARDATAOUT = 0x190
         
         if value:
-            self.gpio_mem[port][GPIO_SETDATAOUT:GPIO_SETDATAOUT+4] = struct.pack("<L", (1 << pin))
+            self._set_bit(port, GPIO_SETDATAOUT, pin, mask = False)
         else:
-            self.gpio_mem[port][GPIO_CLEARDATAOUT:GPIO_CLEARDATAOUT+4] = struct.pack("<L", (1 << pin))
+            self._set_bit(port, GPIO_CLEARDATAOUT, pin, mask = False)
 
-    def read_value(self, port, pin):
-        return True
+    def read_value(self, gpio):
+        port = gpio[0]
+        pin = gpio[1]
+        GPIO_DATAIN = 0x138
+        reg = self._read_reg(port, GPIO_DATAIN)
+        print(reg)
+        value = reg & (1 << pin)
+        return bool(value) 
 
 
 if __name__ == "__main__":
@@ -43,11 +73,16 @@ if __name__ == "__main__":
     gpio = GPIO()
     # (1,28) is port 1, pin 28
     gpio.set_output((1,28))
-
+    gpio.set_input((1,18))
     try:
       while(True):
         gpio.set_value((1,28),1)
+        time.sleep(.5)
+
+        print('pin 14: {}'.format(gpio.read_value((1,18))))
         gpio.set_value((1,28),0)
+        time.sleep(.5)
+        print('pin 14: {}'.format(gpio.read_value((1,18))))
 
 
     except KeyboardInterrupt:
